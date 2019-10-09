6506f2c21a8377d09927a658114308f19bbf47fa
永信 李 <liyongxin@chinatelecom.cn>
2019-10-09 09:30:07 +0800

    add deploy

--- src/cn/ctyun/devops/Deploy.groovy
+++ src/cn/ctyun/devops/Deploy.groovy
@@ -1,8 +1,13 @@
 package cn.ctyun.devops
 
 def deploy(String dir) {
+    this.dir = dir
+    //todo for more param support
+}
+
+def start() {
     try {
-        sh "kubectl apply -f ${dir}"
+        sh "kubectl apply -f ${this.dir}"
     } catch (Exception exc) {
         echo "failed to deploy,exception: ${exc}."
     }
