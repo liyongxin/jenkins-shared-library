2c76280d9ac52a23957bc0de5d5c069181b312fd
永信 李 <liyongxin@chinatelecom.cn>
2019-10-09 16:34:07 +0800

    add test dir

--- test/YamlTest.groovy
+++ test/YamlTest.groovy
@@ -3,5 +3,5 @@ import org.yaml.snakeyaml.Yaml
 
 Yaml parser = new Yaml()
 HashMap example = parser.load(("ingress.yaml" as File).text)
-
+println(example["kind"])
 //example.each{println it.subject}
