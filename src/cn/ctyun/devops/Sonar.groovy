41411ecf1cb35fd8b7348944edb15a2b64dce9d5
永信 李 <liyongxin@chinatelecom.cn>
2019-11-04 17:59:50 +0800

    update notification

--- src/cn/ctyun/devops/Sonar.groovy
+++ src/cn/ctyun/devops/Sonar.groovy
@@ -65,13 +65,14 @@ def startToSonar(install=false) {
             if (qg.status != 'OK') {
                 error "Pipeline aborted due to quality gate failure: ${qg.status}"
                 env.BUILD_RESULT += "Code Scan Failed|"
+
+                updateGitlabCommitStatus(name: 'SonarQube analysis', state: 'failed')
+            }else{
+                env.BUILD_RESULT += "Code Scan OK|"
                 sh """
                     echo 123456
                     echo ${env.BUILD_RESULT}
                 """
-                updateGitlabCommitStatus(name: 'SonarQube analysis', state: 'failed')
-            }else{
-                env.BUILD_RESULT += "Code Scan OK|"
                 updateGitlabCommitStatus(name: 'SonarQube analysis', state: 'success')
             }
         }
