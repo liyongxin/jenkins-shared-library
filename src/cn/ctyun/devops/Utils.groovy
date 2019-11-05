6f4710b7cd71e885541b8b521d0b5a2d385f9267
永信 李 <liyongxin@chinatelecom.cn>
2019-11-05 09:37:09 +0800

    fix notification format

--- src/cn/ctyun/devops/Utils.groovy
+++ src/cn/ctyun/devops/Utils.groovy
@@ -1,6 +1,6 @@
 package cn.ctyun.devops
 
 def updateBuildMessage(String source, String add) {
-    env.BUILD_RESULT = source + add + "\n\t&nbsp;"
+    env.BUILD_RESULT = source + add + "\n&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
     return env.BUILD_RESULT
 }
