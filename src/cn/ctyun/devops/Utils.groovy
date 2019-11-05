c032cc8c442123a20aa4a840219da917395edc0a
永信 李 <liyongxin@chinatelecom.cn>
2019-11-05 09:34:50 +0800

    update notification

--- src/cn/ctyun/devops/Utils.groovy
+++ src/cn/ctyun/devops/Utils.groovy
@@ -1,6 +1,6 @@
 package cn.ctyun.devops
 
 def updateBuildMessage(String source, String add) {
-    env.BUILD_RESULT = source + add + "\n\t"
+    env.BUILD_RESULT = source + add + "\n\t&nbsp;"
     return env.BUILD_RESULT
 }
