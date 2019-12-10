a26be9717dcda7752e276e2bb42b221bf94a1054
永信 李 <liyongxin@chinatelecom.cn>
2019-12-10 18:57:57 +0800

    fix msg format

--- src/cn/ctyun/devops/Utils.groovy
+++ src/cn/ctyun/devops/Utils.groovy
@@ -2,6 +2,6 @@ package cn.ctyun.devops
 
 def updateBuildMessage(String source, String add) {
     //env.BUILD_RESULT = source + add + "\n&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
-    env.BUILD_RESULT = source + add + "\n                    \n"
+    env.BUILD_RESULT = source + add + "\n                    \n&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
     return env.BUILD_RESULT
 }
