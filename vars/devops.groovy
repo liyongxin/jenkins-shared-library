9a58933d41d2e1ea1c26892522e692e6f3c7be33
永信 李 <liyongxin@chinatelecom.cn>
2020-03-25 10:04:47 +0800

    fix synx

--- vars/devops.groovy
+++ vars/devops.groovy
@@ -218,7 +218,7 @@ def acceptTest(comp=""){
 }
 
 static String checkLastCommitPath(String path="") {
-    def val = sh(returnStdout: true, script: "git log --pretty=format:"" --name-only  -1|grep ${path}")
+    def val = sh(returnStdout: true, script: "git log --pretty=format:'' --name-only  -1|grep ${path}")
     if (val != "") {
         return "1"
     }
